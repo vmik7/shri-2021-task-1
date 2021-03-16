bf9ea3325f3b2629116a9e3ff7edfcaf405fea2d
vmik7 <vmik7@yandex.ru>
2021-03-16 15:20:27 +0300

    Установка EJS

--- index.js
+++ index.js
@@ -5,12 +5,21 @@ const __dirname = path.resolve();
 const PORT = 8080;
 const app = express();
 
+// Подключаем шаблонизатор
+app.set('view engine', 'ejs');
+console.log(app.get('views'));
+
+// Делаем папку build статичной
 app.use(express.static(path.resolve(__dirname, 'build')));
 
+// Обрабатываем корневой запрос
 app.get('/', (req, res) => {
-    res.send('<h1>Hello world!</h1>');
+    res.render('index', {
+        message: 'This is current message...'
+    });
 });
 
+// Слушаем порт
 app.listen(PORT, () => {
     console.log(`Server has been started... port: ${PORT}`);
 });
