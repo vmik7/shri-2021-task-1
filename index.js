57401528bfda90998bb9335ce021548dba8df0d8
vmik7 <vmik7@yandex.ru>
2021-03-16 15:06:22 +0300

    Папка build теперь статична

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 import express from 'express'
+import path from 'path'
 
+const __dirname = path.resolve();
+const PORT = 8080;
 const app = express();
-app.listen(8080, () => {
-    console.log('Server has been started...');
+
+app.use(express.static(path.resolve(__dirname, 'build')));
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello world!</h1>');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server has been started... port: ${PORT}`);
 });
